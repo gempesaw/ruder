7a8e85a14fe48f48ce84dabf169b5ec33947a729
Daniel Gempesaw <gempesaw@gmail.com>
2016-11-28 22:46:46 -0500

    Add more parser tests

--- test/parser_test.js
+++ test/parser_test.js
@@ -3,33 +3,61 @@ import parsePayload  from '../lib/parser';
 
 describe('Parsing', () => {
     let text, options;
-    beforeEach( () => {
-        text = `POST http://www.google.com
-Header: Value
 
-{ "json": "body" }`;
-        options = parsePayload(text);
-    });
+    describe('GET', () => {
+        beforeEach(() => {
+            text = 'GET https://www.google.com';
+            options = parsePayload(text);
+        });
 
-    it('should extract the method', () => {
-        expect(options.method).to.equal('POST');
-    });
+        it('should extract the method', () => {
+            expect(options.method).to.equal('GET');
+        });
 
-    it('should extract the url', () => {
-        expect(options.url).to.equal('http://www.google.com');
-    });
+        it('should extract the url', () => {
+            expect(options.url).to.equal('https://www.google.com');
+        });
 
-    it('should extract the header', () => {
-        expect(options.headers).to.eql({
-            Header: 'Value'
+        it('should not specify a body or json options', () => {
+            expect(options.body).to.be.undefined;
+            expect(options.json).to.be.undefined;
         });
     });
 
-    it('should extract the body', () => {
-        expect(options.body).to.eql({json: 'body'});
-    });
+    describe('POST', () => {
+        beforeEach( () => {
+            text = `POST http://www.google.com
+Header1: Value1
+Header2: Value2
 
-    it('should set the appropriate json header', () => {
-        expect(options.json).to.be.true;
+{ "json": "body", "json2": "body"}`;
+            options = parsePayload(text);
+        });
+
+        it('should extract the method', () => {
+            expect(options.method).to.equal('POST');
+        });
+
+        it('should extract the url', () => {
+            expect(options.url).to.equal('http://www.google.com');
+        });
+
+        it('should extract the header', () => {
+            expect(options.headers).to.eql({
+                Header1: 'Value1',
+                Header2: 'Value2'
+            });
+        });
+
+        it('should extract the body', () => {
+            expect(options.body).to.eql({
+                json: 'body',
+                json2: 'body'
+            });
+        });
+
+        it('should set the appropriate json header', () => {
+            expect(options.json).to.be.true;
+        });
     });
 });
